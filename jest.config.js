6284f49a1079eceb377fd2e9f9c997817034a170
Jeremiah Duhart <jaydu1995@gmail.com>
2023-04-19 15:47:57 -0400

    (chore) Add and set up another test dependency (testing-library/jest-dom). Fix collision between Cypress and Jest.

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
-const nextJest = require('next/jest');
+const nextJest = require("next/jest");
 
-const createJestConfig = nextJest({dir: './'});
+const createJestConfig = nextJest({ dir: "./" });
 
 const customJestConfig = {
-  moduleDirectories: ['node_modules', '<rootDir>/'],
-  testEnvironment: 'jest-environment-jsdom',
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
 };
 
 module.exports = createJestConfig(customJestConfig);
